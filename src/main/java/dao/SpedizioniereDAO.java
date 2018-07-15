1897329bea387f699a508fb31b94f3824f2b91b4
Eliander <piacentinielia@gmail.com>
2018-07-15 21:12:48 +0200

    Aggiunto getAllSpedizionieri

--- src/main/java/dao/SpedizioniereDAO.java
+++ src/main/java/dao/SpedizioniereDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import model.Indirizzo;
 import model.Spedizioniere;
 import org.apache.logging.log4j.LogManager;
@@ -15,6 +16,7 @@ public class SpedizioniereDAO {
 
     private static org.apache.logging.log4j.Logger log = LogManager.getLogger(SpedizioniereDAO.class);
 
+    private final String SELECT = "SELECT * FROM SPEDIZIONIERE";
     private final String SELECTBYNAME = "SELECT * FROM SPEDIZIONIERE WHERE nome = ?";
 
     public Spedizioniere getSpedizioniereByNome(String nome) {
@@ -31,6 +33,20 @@ public class SpedizioniereDAO {
         }
         return spedizioniere;
     }
+    
+    public ArrayList<Spedizioniere> getSpedizionieri() {
+        ArrayList<Spedizioniere> spedizionieri = null;
+        try {
+            Connection con = DAOSettings.getConnection();
+            PreparedStatement pst = con.prepareStatement(SELECT);
+            ResultSet resultset = pst.executeQuery();
+            spedizionieri = mapRowToArrayListSpedizionieri(resultset);
+            con.close();
+        } catch (Exception ex) {
+            log.error(ex);
+        }
+        return spedizionieri;
+    }
 
     private Spedizioniere mapRowToSpedizioniere(ResultSet resultset) {
         Spedizioniere spedizioniere = null;
@@ -44,6 +60,22 @@ public class SpedizioniereDAO {
             log.error(ex);
         }
         return spedizioniere;
+    }
+    
+    private ArrayList<Spedizioniere> mapRowToArrayListSpedizionieri(ResultSet resultset) {
+        ArrayList<Spedizioniere> spedizionieri = new ArrayList();
+        Spedizioniere spedizioniere = null;
+        try {
+            while (resultset.next()) {
+                spedizioniere = new Spedizioniere(resultset.getString("nome"),
+                        new Indirizzo(resultset.getString("citta"), resultset.getString("via"), resultset.getInt("numero")),
+                        resultset.getString("telefono"));
+                spedizionieri.add(spedizioniere);
+            }
+        } catch (Exception ex) {
+            log.error(ex);
+        }
+        return spedizionieri;
     }
 
 }
