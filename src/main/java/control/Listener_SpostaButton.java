adca526ab18f011de648db38ad291c94c51cefe3
Bosky <Bosky@PC-FEDE-WORK>
2018-07-15 11:39:53 +0200

    SpostaArticoli senza chiamate DAO

---
+++ src/main/java/control/Listener_SpostaButton.java
@@ -0,0 +1,43 @@
+package control;
+
+import dao.DAOSettings;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.ArticoloMagazzino;
+import model.Utente;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import view.View_SpostaArticoli;
+
+/**
+ *
+ * @author Bosky
+ */
+public class Listener_SpostaButton implements ActionListener{
+    private final static Logger log = LogManager.getLogger(Listener_SpostaButton.class);
+    private View_SpostaArticoli frame;
+    
+    private static DAOSettings DAO = Main.getDAO();
+
+    public Listener_SpostaButton(View_SpostaArticoli frame) {
+        this.frame = frame;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ArticoloMagazzino artmag = this.frame.getSelectedArticolo();
+        int scaffale = this.frame.getSelectedScaffale();
+        int ripiano = this.frame.getSelectedRipiano();
+        if(artmag != null){
+            //TO DO
+            //cambiare da DAO la posizione
+            //if(DAO.getArticoloMagazzino().setPosition(new Posizione(scaffale,ripiano))){
+                Utente user = this.frame.getUser();
+                this.frame.dispose();
+                View_SpostaArticoli view_SpostaArticoli = new View_SpostaArticoli(user);
+                view_SpostaArticoli.setVisible(true);
+                System.out.println("Articolo spostato");
+            //}
+        }
+    }
+}
--- src/main/java/view/View_SpostaArticoli.java
+++ src/main/java/view/View_SpostaArticoli.java
@@ -1,8 +1,23 @@
 package view;
 
+import control.Listener_BackToHomeMagazziniereButton;
+import control.Listener_SpostaButton;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.WindowConstants;
+import model.ArticoloMagazzino;
 import model.Utente;
 
 /**
@@ -11,6 +26,19 @@ import model.Utente;
  */
 public class View_SpostaArticoli extends JFrame{
     private Utente user = null;
+    
+    private JLabel label_title;
+    private JPanel btn_panel;
+    private JButton button_back;
+    private JLabel label_sel_articolo;
+    private JList list_articoli;
+    private JScrollPane list_articoli_scroller;
+    private JLabel label_scaffale;
+    private JSpinner text_scaffale;
+    private JLabel label_ripiano;
+    private JSpinner text_ripiano;
+    private JButton button_sposta;
+    private JPanel btn_panel2;
     
     public View_SpostaArticoli(Utente user) {
         this.user = user;
@@ -22,10 +50,76 @@ public class View_SpostaArticoli extends JFrame{
         setPreferredSize(new Dimension(600, 500));
         setResizable(false);
         
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(new GridLayout(9,1));
+
+        button_back = new JButton();
+        button_back.setText("INDIETRO");
+        button_back.addActionListener(new Listener_BackToHomeMagazziniereButton(this));
+        btn_panel = new JPanel();
+        btn_panel.add(button_back);
+        btn_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        contentPane.add(btn_panel);
+        
+        label_title = new JLabel();
+        label_title.setText("SPOSTA ARTICOLI");
+        contentPane.add(label_title);
+        
+        label_sel_articolo = new JLabel();
+        label_sel_articolo.setText("Seleziona articolo:");
+        contentPane.add(label_sel_articolo);
+        
+        //TO DO
+        //ricavo da DAO (tramite query) la lista di tutti gli ArticoloMagazzino
+        //ArrayList<ArticoloMagazzino> artmag = DAO.getArticoloMagazzinoDAO().getAllArticoloMagazzino();
+        //ArticoloMagazzino[] articoli_mag = new ArticoloMagazzino[artmag.size()];
+        //articoli_mag = artmag.toArray(articoli_mag);
+        list_articoli = new JList(); //passare come parametro l'array articoli_mag
+        list_articoli.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list_articoli.setLayoutOrientation(JList.VERTICAL);
+        list_articoli.setVisibleRowCount(10);
+        list_articoli_scroller = new JScrollPane(list_articoli);
+        list_articoli_scroller.setPreferredSize(new Dimension(100,100));
+        contentPane.add(list_articoli_scroller);
+        
+        label_scaffale = new JLabel();
+        label_scaffale.setText("Scaffale:");
+        contentPane.add(label_scaffale);
+        SpinnerModel model = new SpinnerNumberModel(1,1,1000,1);
+        text_scaffale = new JSpinner(model);
+        contentPane.add(text_scaffale);
+        
+        label_ripiano = new JLabel();
+        label_ripiano.setText("Ripiano:");
+        contentPane.add(label_ripiano);
+        SpinnerModel model2 = new SpinnerNumberModel(1,1,1000,1);
+        text_ripiano = new JSpinner(model2);
+        contentPane.add(text_ripiano);
+        
+        button_sposta = new JButton();
+        button_sposta.setText("Sposta");
+        button_sposta.addActionListener(new Listener_SpostaButton(this));
+        btn_panel2 = new JPanel();
+        btn_panel2.add(button_sposta);
+        btn_panel2.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        contentPane.add(btn_panel2);
+        
         this.pack();
     }
     
     public Utente getUser(){
         return this.user;
+    }
+    
+    public ArticoloMagazzino getSelectedArticolo(){
+        return (ArticoloMagazzino)this.list_articoli.getSelectedValue();
+    }
+    
+    public int getSelectedScaffale(){
+        return (Integer)this.text_scaffale.getValue();
+    }
+    
+    public int getSelectedRipiano(){
+        return (Integer)this.text_ripiano.getValue();
     }
 }
