51f97143b4546dbcbd90ae62a82bbfffdbe079dc
Bosky <Bosky@PC-FEDE-WORK>
2018-07-16 12:40:43 +0200

    in ViewDettagliOrdine visualizzo lo stato dell'uscita

--- src/main/java/view/View_DettagliOrdine.java
+++ src/main/java/view/View_DettagliOrdine.java
@@ -1,6 +1,8 @@
 package view;
 
 import control.Listener_BackToOrdiniPassati;
+import control.Main;
+import dao.DAOSettings;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -15,6 +17,7 @@ import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 import model.ArticoloOrdinato;
 import model.Ordine;
+import model.Uscita;
 
 /**
  *
@@ -22,8 +25,7 @@ import model.Ordine;
  */
 public class View_DettagliOrdine extends JFrame{
     private Ordine ordine;
-    private JPanel dati_panel;
-    private JPanel lista_panel;
+    
     private JPanel btn_panel;
     private JButton button_back;
     private JLabel label_title;
@@ -35,6 +37,13 @@ public class View_DettagliOrdine extends JFrame{
     private JLabel label_articoliOrdinati;
     private JList list;
     private JScrollPane list_scroller;
+    private JLabel label_stato;
+    private JLabel label_dettagli_uscita;
+    private JLabel label_bolla;
+    private JLabel label_data_uscita;
+    private JLabel label_spedizioniere;
+    
+    private static DAOSettings DAO = Main.getDAO();
 
     public View_DettagliOrdine(Ordine ordine) {
         this.ordine = ordine;
@@ -47,41 +56,43 @@ public class View_DettagliOrdine extends JFrame{
         setResizable(false);
         
         Container contentPane = this.getContentPane();
-        contentPane.setLayout(new GridLayout(1,2));
+        contentPane.setLayout(new GridLayout(14,1));
         
         button_back = new JButton();
         button_back.setText("INDIETRO");
         button_back.addActionListener(new Listener_BackToOrdiniPassati(this));
-        
-        label_title = new JLabel();
-        label_title.setText("DETTAGLI ORDINE");
-        label_codice = new JLabel();
-        label_codice.setText("Codice: " + ordine.getCodice());
-        label_data = new JLabel();
-        label_data.setText("Data dell'ordine: " + ordine.getData().toString());
-        label_codFiscale = new JLabel();
-        label_codFiscale.setText("Codice fiscale negozio: " + ordine.getNegozio().getCodiceFiscale());
-        label_nomeNegozio = new JLabel();
-        label_nomeNegozio.setText("Nome negozio: " + ordine.getNegozio().getNome());
-        label_indirizzoNegozio = new JLabel();
-        label_indirizzoNegozio.setText("Indirizzo negozio: " + ordine.getNegozio().getIndirizzo().toString());
-        
-        dati_panel = new JPanel();
-        dati_panel.setLayout(new GridLayout(7,1));
         btn_panel = new JPanel();
         btn_panel.add(button_back);
         btn_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        dati_panel.add(btn_panel);
-        dati_panel.add(label_title);
-        dati_panel.add(label_codice);
-        dati_panel.add(label_data);
-        dati_panel.add(label_codFiscale);
-        dati_panel.add(label_nomeNegozio);
-        dati_panel.add(label_indirizzoNegozio);
-        contentPane.add(dati_panel);
+        contentPane.add(btn_panel);
         
+        label_title = new JLabel();
+        label_title.setText("DETTAGLI ORDINE");
+        contentPane.add(label_title);
+        
+        label_codice = new JLabel();
+        label_codice.setText("Codice: " + ordine.getCodice());
+        contentPane.add(label_codice);
+        
+        label_data = new JLabel();
+        label_data.setText("Data dell'ordine: " + ordine.getData().toString());
+        contentPane.add(label_data);
+        
+        label_codFiscale = new JLabel();
+        label_codFiscale.setText("Codice fiscale negozio: " + ordine.getNegozio().getCodiceFiscale());
+        contentPane.add(label_codFiscale);
+        
+        label_nomeNegozio = new JLabel();
+        label_nomeNegozio.setText("Nome negozio: " + ordine.getNegozio().getNome());
+        contentPane.add(label_nomeNegozio);
+        
+        label_indirizzoNegozio = new JLabel();
+        label_indirizzoNegozio.setText("Indirizzo negozio: " + ordine.getNegozio().getIndirizzo().toString());
+        contentPane.add(label_indirizzoNegozio);
+
         label_articoliOrdinati = new JLabel();
         label_articoliOrdinati.setText("Articoli ordinati:");
+        contentPane.add(label_articoliOrdinati);
         
         //ottengo la lista di ArticoloOrdinato
         ArticoloOrdinato[] articoli = new ArticoloOrdinato[ordine.getArticoli().size()];
@@ -92,12 +103,38 @@ public class View_DettagliOrdine extends JFrame{
         list.setVisibleRowCount(10);
         list_scroller = new JScrollPane(list);
         list_scroller.setPreferredSize(new Dimension(100,100));
+        contentPane.add(list_scroller);
         
-        lista_panel = new JPanel();
-        lista_panel.setLayout(new GridLayout(2,1));
-        lista_panel.add(label_articoliOrdinati);
-        lista_panel.add(list_scroller);
-        contentPane.add(lista_panel);
+        //ricavo l'uscita dell'ordine da DAO
+        Uscita usc = DAO.getUscitaDAO().getUscitaByOrdine(ordine);
+        
+        if(usc != null){
+            
+            label_stato = new JLabel();
+            label_stato.setText("Stato ordine: Uscito");
+            contentPane.add(label_stato);
+            
+            label_dettagli_uscita = new JLabel();
+            label_dettagli_uscita.setText("DETTAGLI USCITA");
+            contentPane.add(label_dettagli_uscita);
+            
+            label_bolla = new JLabel();
+            label_bolla.setText("Numero di bolla: " + usc.getNumBolla());
+            contentPane.add(label_bolla);
+
+            label_data_uscita = new JLabel();
+            label_data_uscita.setText("Data uscita: " + usc.getData().toString());
+            contentPane.add(label_data_uscita);
+
+            label_spedizioniere = new JLabel();
+            label_spedizioniere.setText("Spedizioniere: " + usc.getSpedizioniere().toString());
+            contentPane.add(label_spedizioniere);
+        }
+        else{
+            label_stato = new JLabel();
+            label_stato.setText("Stato ordine: In elaborazione");
+            contentPane.add(label_stato);
+        }
         
         this.pack();
     }
